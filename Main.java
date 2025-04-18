49ff02405bb210326267bf394979030116991850
codeneat9 <yashwithbehara18@gmail.com>
2025-04-18 22:56:51 +0530

    Initializing Switch case

--- Main.java
+++ Main.java
@@ -7,3 +7,22 @@ Batch:AIML B3
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StudentManager manager = new StudentManager();
+
+        int choice;
+        do {
+            System.out.println("\n===== Student Data Entry Menu =====");
+            System.out.println("1. Add Student");
+            System.out.println("2. Display All Students");
+            System.out.println("3. Search by PRN");
+            System.out.println("4. Search by Name");
+            System.out.println("5. Search by Position");
+            System.out.println("6. Update Student");
+            System.out.println("7. Delete Student");
+            System.out.println("0. Exit");
+            System.out.print("Enter your choice: ");
+            choice = sc.nextInt();
+            sc.nextLine(); // clear buffer
+
