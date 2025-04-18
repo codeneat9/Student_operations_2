ed1cfef63bf383de5e743b1a472e5fdd00ca62c7
codeneat9 <yashwithbehara18@gmail.com>
2025-04-18 22:59:33 +0530

    Student Class assignment

--- Student.java
+++ Student.java
@@ -4,3 +4,10 @@ public class Student {
     String dob;
     float marks;
 
+    public Student(String prn, String name, String dob, float marks) {
+        this.prn = prn;
+        this.name = name;
+        this.dob = dob;
+        this.marks = marks;
+    }
+
